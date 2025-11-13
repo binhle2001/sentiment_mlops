26edb8899533e319b3314673e6491202d30255ee
binhle2001 <minhan012710@gmail.com>
2025-11-13 15:28:22 +0700

    fix host

--- migrate_ids_to_int.py
+++ migrate_ids_to_int.py
@@ -34,11 +34,11 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
-DB_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
-DB_NAME = os.getenv("POSTGRES_DB", "label_db")
-DB_USER = os.getenv("POSTGRES_USER", "postgres")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+DB_HOST = 'localhost'
+DB_PORT = 5499
+DB_NAME = 'label_db'
+DB_USER =  'postgres'
+DB_PASSWORD = 'qwertyxxx'
 
 
 def get_connection() -> psycopg2.extensions.connection:
